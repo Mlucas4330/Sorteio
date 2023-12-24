3fb1b442371cda6e3d5b614dfef8d94ff21ee438
mlucas4330 <mlucas@gmail.com>
2023-12-24 00:12:10 -0300

    commit parcial

--- server/src/config/databaseConfig.js
+++ server/src/config/databaseConfig.js
@@ -3,11 +3,16 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: 'localhost',
+const DB_NAME = process.env.NODE_ENV === 'development' ? process.env.DEV_DB_NAME : process.env.PROD_DB_NAME
+
+const db = new Sequelize({
+  host: process.env.DB_HOST,
   dialect: 'postgres',
   port: 5432,
-  logging: process.env.DB_LOG === 'true'
+  logging: process.env.DB_LOG === 'true',
+  password: process.env.DB_PASS,
+  username: process.env.DB_USER,
+  database: DB_NAME
 })
 
 export default db
