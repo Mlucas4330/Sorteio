a3bb24b673ca193a4e33de30e6fb86189b34e4a6
Lucas Medeiros <mlucas4330@gmail.com>
2024-02-17 03:41:59 -0300

    add eslint

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,44 @@
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Home, SignIn, SignUp, Tutorial, ChangePassword, ForgotPassword, ResetPassword } from './pages'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />,
+    },
+    {
+        path: '/signin',
+        element: <SignIn />,
+    },
+    {
+        path: '/signup',
+        element: <SignUp />,
+    },
+    {
+        path: '/tutorial',
+        element: <Tutorial />,
+    },
+    {
+        path: '/change-password',
+        element: <ChangePassword />,
+    },
+    {
+        path: '/forgot-password',
+        element: <ForgotPassword />,
+    },
+    {
+        path: '/reset-password/:token?',
+        element: <ResetPassword />,
+    },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <StrictMode>
         <ChakraProvider>
             <RouterProvider router={router} />
         </ChakraProvider>
-    </StrictMode>
+    </StrictMode>,
 )
